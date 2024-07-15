754063a1231f03e46b505d65ba7f0c9e8a2bd353
Hermes José Pérez Rojas <hermesjose94@gmail.com>
2024-07-14 22:29:56 -0500

    feat: new migrations 2

--- supabase/migrations/20240715032237_edit_table_players.sql
+++ supabase/migrations/20240715032237_edit_table_players.sql
@@ -1,7 +1,5 @@
 alter table "public"."players" drop column "number";
 
-alter table "public"."players" drop column "position";
-
 alter table "public"."players" add column "firstname" text not null;
 
 alter table "public"."players" add column "lastname" text not null;
